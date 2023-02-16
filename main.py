514f219a324f8c0009dd5601d1356213308410d8
philipsteingruber <philip.steingruber@gmail.com>
2023-02-16 12:37:35 +0100

    Fancy cursor

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ class Main:
 
 		self.editor = Editor()
 
+		cursor_surface = pygame.image.load('./graphics/cursors/mouse.png').convert_alpha()
+		cursor = pygame.cursors.Cursor((0, 0), cursor_surface)
+		pygame.mouse.set_cursor(cursor)
+
 	def run(self):
 		while True:
 			dt = self.clock.tick() / 1000
